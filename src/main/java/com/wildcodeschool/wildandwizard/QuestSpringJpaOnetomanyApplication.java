4f9fe5914229d29192f381b5fe0409cf2ea9de5c
john Doe <sataward666@gmail.com>
2022-11-16 14:49:25 +0100

    aie

---
+++ src/main/java/com/wildcodeschool/wildandwizard/QuestSpringJpaOnetomanyApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.wildandwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestSpringJpaOnetomanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestSpringJpaOnetomanyApplication.class, args);
+	}
+
+}
